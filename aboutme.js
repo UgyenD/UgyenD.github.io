c757f73c70d9132990beed9cfdeb058ea0cb4fc7
UgyenD <tddorji101@gmail.com>
2019-10-10 05:13:19 -0700

    Completed majority of site
    Major changes to index.html,aboutme.html and main.css. Still need to add scrolling effects for more dynamic website.

---
+++ aboutme.js
@@ -0,0 +1,34 @@
+//$('#aboutmeDiv').css('min-height', '100%');
+//var aboutmeWindow = $("#aboutmeDiv");
+//
+//var aboutmeHeight = aboutmeWindow.outerHeight();
+//
+//var aboutmeWidth = aboutmeWindow.outerWidth;
+//
+//var aboutmeBody = $("#aboutmeimgdiv");
+//
+//aboutmeBody.resizable({
+//    resize: doResize
+//});
+//
+//
+//function doResize(event, ui)
+//{
+//    var scale, origin;
+//    
+//    scale = Math.min(
+//    ui.size.width / aboutmeWidth, ui.size.height /aboutmeHeight);
+//    
+//    aboutmeWindow.css({
+//        transform:"translate(-50%,-50%)"+"scale(" + scale +")"
+//    });
+//}
+//
+//var starterData = { 
+//  size: {
+//    width: aboutmeBody.width(),
+//    height: aboutmeBody.height()
+//  }
+//}
+//doResize(null, starterData);
+//
